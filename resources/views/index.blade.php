a6d531e2c729b79d03a4304d1c742c55040df2b3
Deev Maksim <deev.maks2012@ya.ru>
2019-10-28 14:14:38 +0500

    Auth created

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app', ['page_title' => 'Welcome!'])
 
 @section('html_header')
-<!-- Additional header tags -->
+
 @endsection
 
 @section('main_content')
--- resources/views/layouts/partials/main_header.blade.php
+++ resources/views/layouts/partials/main_header.blade.php
@@ -1,1 +1,23 @@
-{{-- Main header --}}
+<nav class="navbar navbar-default">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="{{ route('home') }}">Avtocod | Стена сообщений</a>
+        </div>
+        @guest
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="{{ route('home') }}">Главная</a></li>
+                <li><a href="{{ route('login') }}">Авторизация</a></li>
+                <li><a href="{{ route('register') }}">Регистрация</a></li>
+            </ul>
+        @endguest
+        @auth
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="navbar-text"><span class="glyphicon glyphicon-user"></span> {{ Auth::user()->name }}</li>
+                <li><a href="#"
+                onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><span class="glyphicon glyphicon-log-out"></span> Выход</a></li>
+                {{-- @todo  --}}
+            </ul>
+        @endauth
+    </div>
+</nav>
